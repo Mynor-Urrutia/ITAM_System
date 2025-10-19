d3d7a0a0e7d19de460191221ce3e78678937ac81
Mynor-Urrutia <myjourrutia@gmail.com>
2025-10-18 23:31:12 -0600

    Fix media URL serving configuration

--- itam_backend/urls.py
+++ itam_backend/urls.py
@@ -3,7 +3,7 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+#from django.conf.urls.static import static
 from django.http import HttpResponse
 from django.views.static import serve
 from rest_framework_simplejwt.views import (
